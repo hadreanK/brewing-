d3fc23dfbb6a429959100866680639a1e55a6b9d
Adrian Kirn <kirnadrian@gmail.com>
2017-12-15 12:34:24 -0800

    Started writing class definitions

--- helper-brewing.h
+++ helper-brewing.h
@@ -2,10 +2,31 @@
 #define helper-brewing_h
 
 // INCLUDES
-#include "AVR-Timers"
+#include <AVR-Timers>
 
 // CLASS AND FUNCTION DEFINITIONS
 
+// BeerThermometer will set up a thermometer using the oneWire, read the thermometer and print out the Temperature in °F
+
+class BeerThermometer{
+		int pin;
+		int temp;
+	public:
+		void setup();
+		void getTemp();
+		float printTemp();
+};
+
+// BeerTimer will set timers to go off after a certain amount of time has passed
+class BeerTimer{
+		int setTime; // The time the alarm is set for
+	public:
+		void setTimer(); 	// This will set the timer
+		int timeLeft(); 	// returns the time left in seconds
+		
+		
+	
+};
 
 
 
